f3cc16c9ea98f680c4ac1e124479ae281bcac27e
Prince <justprince9@gmail.com>
2021-01-30 22:37:29 +0400

    test theming

---
+++ src/theme.js
@@ -0,0 +1,13 @@
+// theme.js
+export const lightTheme = {
+    body: "#fafafa",
+    text: "#555555",
+    boldText: "#333333",
+    toggleBorder: "",
+};
+
+export const darkTheme = {
+    body: "#363537",
+    text: "#ffffff",
+    boldText: "#ffffff",
+};
