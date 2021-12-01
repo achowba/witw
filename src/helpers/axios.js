bc03eb7778b83686505203933e74fcd2c0ef3cab
Prince <justprince9@gmail.com>
2021-12-01 09:06:35 +0400

    Fixed base URL 404 issue

--- src/helpers/axios.js
+++ src/helpers/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: 'https://restcountries.com/v3.1/all',
+    baseURL: 'https://restcountries.com/v3.1/',
 });
 
 export default instance;
